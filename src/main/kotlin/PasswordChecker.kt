ab23500a3ac4a824566bc97911ba6a42a2c66fc4
xibalbam <maelporret@outlook.fr>
2024-11-09 16:33:52 +0100

    Initial commit

--- src/main/kotlin/PasswordChecker.kt
+++ src/main/kotlin/PasswordChecker.kt
@@ -7,9 +7,41 @@ class PasswordChecker(private val requiredConditions: List<PasswordCondition>,
     }
 }
 
-data class PasswordCheckResult(val isRespectingConditions: Boolean, val strength: Int, val failedConditions: List<String>)
+data class PasswordCheckResult(val isRespectingConditions: Boolean, val strength: Int, val failedConditions: List<String>) {
+    override fun toString(): String {
+        return if (isRespectingConditions) {
+            "Password respect conditions, strength: $strength"
+        } else {
+            "Password don't respect conditions, strength: $strength\nFailed conditions: \n - ${failedConditions.joinToString("\n - ")}"
+        }
+    }
+}
 
-data class PasswordCondition(val name: String, val check: (String) -> Boolean)
+data class PasswordCondition(val name: String, val check: (String) -> Boolean) {
+    companion object {
+        fun lengthBetween(min: Int, max: Int) = PasswordCondition("Length must be between $min and $max characters") { it.length in min..max }
+        fun uppercaseLetter() = PasswordCondition("Must contain an uppercase letter") { it.any { it.isUpperCase() } }
+        fun lowercaseLetter() = PasswordCondition("Must contain an lowercase letter") { it.any { it.isLowerCase() } }
+        fun digit() = PasswordCondition("Must contain a digit") { it.any { it.isDigit() } }
+        fun specialCharacter() = PasswordCondition("Must contain a special character") { it.any { !it.isLetterOrDigit() } }
+        fun noCommonWords(words: List<String>) = PasswordCondition("Must not contain common words") { password ->
+            words.none { password.contains(it, ignoreCase = true) }
+        }
+        fun noSequence() = PasswordCondition("Must not contain a character sequence") { password ->
+            if (password.length < 4) {
+                return@PasswordCondition true
+            }
+            for (i in 0 until password.length - 4) {
+                if (password[i] + 1 == password[i + 1] && password[i + 1] + 1 == password[i + 2] && password[i + 2] + 1 == password[i + 3]) {
+                    return@PasswordCondition false
+                }
+            }
+            true
+        }
+        fun regex(name: String, regex: Regex) = PasswordCondition(name) { regex.containsMatchIn(it) }
+        fun noRegex(name: String, regex: Regex) = PasswordCondition(name) { !regex.containsMatchIn(it) }
+    }
+}
 
 fun interface StrengthFunction {
     fun calculate(password: String): Int
--- src/main/kotlin/Test.kt
+++ src/main/kotlin/Test.kt
@@ -2,32 +2,14 @@ import kotlin.math.floor
 import kotlin.math.min
 
 val passwordChecker = PasswordChecker(listOf(
-    PasswordCondition("Length must be between 8 and 32 characters") { it.length in 8..32 },
-    PasswordCondition("Must contain an uppercase letter") { it.any { it.isUpperCase() } },
-    PasswordCondition("Must contain an lowercase letter") { it.any { it.isLowerCase() } },
-    PasswordCondition("Must contain a digit") { it.any { it.isDigit() } },
-    PasswordCondition("Must contain a special character") { it.any { !it.isLetterOrDigit() } },
-    PasswordCondition("Must not contain an obvious sequence") { password ->
-        val bannedRegexs = listOf(
-            Regex("(\\w)\\1{3,}"), // 4 or more identical characters in a row
-        )
-        bannedRegexs.none { it.containsMatchIn(password) }
-    },
-    PasswordCondition("Must not contain common words") { password ->
-        val bannedWords = object {}.javaClass.getResource("/common-passwords.txt")!!.readText().lines()
-        bannedWords.none { password.contains(it, ignoreCase = true) }
-    },
-    PasswordCondition("Must not contain a character sequence") { password ->
-        if (password.length < 4) {
-            return@PasswordCondition true
-        }
-        for (i in 0 until password.length - 4) {
-            if (password[i] + 1 == password[i + 1] && password[i + 1] + 1 == password[i + 2] && password[i + 2] + 1 == password[i + 3]) {
-                return@PasswordCondition false
-            }
-        }
-        true
-    }
+    PasswordCondition.lengthBetween(8, 32),
+    PasswordCondition.lowercaseLetter(),
+    PasswordCondition.uppercaseLetter(),
+    PasswordCondition.digit(),
+    PasswordCondition.specialCharacter(),
+    PasswordCondition.noCommonWords(object {}.javaClass.getResource("/common-passwords.txt")!!.readText().lines()),
+    PasswordCondition.noRegex("Must not contain a repetition of characters", Regex("(.)\\1{3,}")),
+    PasswordCondition.noSequence()
 )) { password ->
     val length = password.length
     val differentCharacters = password.toSet().size
@@ -37,12 +19,6 @@ val passwordChecker = PasswordChecker(listOf(
 fun main() {
     while (true) {
         val password = readln()
-        val check = passwordChecker.check(password)
-        if (check.isRespectingConditions) {
-            println("Password respect conditions, strength: ${check.strength}")
-        } else {
-            println("Password don't respect conditions, strength: ${check.strength}")
-            println("Failed conditions: \n - ${check.failedConditions.joinToString("\n - ")}")
-        }
+        println(passwordChecker.check(password))
     }
 }
